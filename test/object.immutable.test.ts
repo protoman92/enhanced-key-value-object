ee4dca2187ff6edc31e6c7ac6d6d5d880ff6ddfe
Hai Pham <swiften.svc@gmail.com>
2018-06-11 12:02:15 +0800

    test(immutable): add immutability test for copying/moving values

--- test/object.immutable.test.ts
+++ test/object.immutable.test.ts
@@ -3,11 +3,11 @@ import { EKVObject } from './../src';
 
 describe('EK Object should be immutable', () => {
   let object: JSObject<any>;
-  let ekObject: EKVObject.Type;
+  let ekvObject: EKVObject.Type;
 
   beforeEach(() => {
     object = {};
-    ekObject = EKVObject.just(object);
+    ekvObject = EKVObject.just(object);
   });
 
   it('Setting object via builder should deep clone it', () => {
@@ -15,6 +15,23 @@ describe('EK Object should be immutable', () => {
     object.a = 1;
 
     /// Then
-    expect(ekObject.valueAtNode('a').isFailure()).toBeTruthy();
+    expect(ekvObject.valueAtNode('a').isFailure()).toBeTruthy();
+  });
+
+  it('Copying and moving values should not modify original object', () => {
+    /// Setup
+    let sourcePath = 'a.b.c';
+    let destPath = 'e.f.g.h';
+    ekvObject = ekvObject.updatingValue('a.b.c', { d: 1 });
+
+    /// When
+    let copied = ekvObject.copyingValue(sourcePath, destPath);
+    let moved = ekvObject.movingValue(sourcePath, destPath);
+
+    /// Then
+    expect(copied.valueAtNode(sourcePath).isSuccess()).toBeTruthy();
+    expect(copied.valueAtNode(destPath).isSuccess()).toBeTruthy();
+    expect(moved.valueAtNode(sourcePath).isFailure()).toBeTruthy();
+    expect(moved.valueAtNode(destPath).isSuccess()).toBeTruthy();
   });
 });
